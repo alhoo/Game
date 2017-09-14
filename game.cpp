7f63f95108c179668c5349a13f66c6a2db53679f
Lasse Hyyrynen <hyyrynen@lingsoft.fi>
2017-09-14 09:27:26 +0300

    Refactor a drawing function that can push the result to a game engine

--- game.cpp
+++ game.cpp
@@ -10,55 +10,61 @@
 
 using namespace std;
 
-int w = 1024, h=768;
-unsigned char * pixels = new unsigned char[w * h * 3];
+int width = 1024, height=768;
+unsigned char * pixels = new unsigned char[width * height * 3];
 
 class World {
   public:
     World(){
       this->state[0] = 0;
       start = chrono::system_clock::now();
-    };
+    }
+
     void update(){
       auto end = chrono::system_clock::now();
-      float dt = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()*0.000001;
+      float dt = chrono::duration_cast<chrono::microseconds>(end - start).count()*0.000001;
       start = end;
       this->state[0] += 100*dt;
     }
-    void draw(unsigned char * pixels, unsigned short w, unsigned short h){
+
+    void draw(void (*fn)(unsigned char *, int, int),
+              unsigned short width, unsigned short heigth){
       auto k = (((int)(this->state[0]))%3000 + 1000);
-      for (auto i = 0; i < 3 * w * h; i++) {
+      for (auto i = 0; i < 3 * width * heigth; i++) {
         pixels[i] = 0x00;
         if (i%4000 == k){
           pixels[i] = 0xFF;
         }
       }
+      fn(pixels,width,heigth);
     }
+
   private:
      chrono::time_point<chrono::system_clock> start;
      unordered_map<int, int> state;
+
 };
 
-void updateTexture(World world){
-  world.draw(pixels, w, h);
-  glTexSubImage2D(GL_TEXTURE_2D, 0 ,0, 0, w, h, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)pixels);
+void updatePixels(unsigned char * pixels, int width, int heigth){
+  glTexSubImage2D(GL_TEXTURE_2D, 0 ,0, 0, width, heigth,
+                  GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)pixels);
 }
 
 int main(){
 
-    GLFWwindow* window = init(w, h);
+    GLFWwindow* window = init(width, height);
     if(window == NULL) return 1;
     vector<GLuint> texture = loadTexture();
     World w;
     unsigned long frame = 0;
     do{
-        updateTexture(w);
+        w.update();
+        w.draw(updatePixels, width, height);
+        draw(texture);
         // Swap buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
 
-        w.update();
-        draw(texture);
         frame++;
     }
     while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
