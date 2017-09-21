927fde27b39a269d9c387fa67523bc6fa6d31832
Lasse Hyyrynen <hyyrynen@lingsoft.fi>
2017-09-21 13:23:35 +0300

    Calculate people movement in GPU

--- game.cpp
+++ game.cpp
@@ -10,15 +10,20 @@
 
 #include "video.h"
 
+#include <boost/numeric/ublas/matrix.hpp>
+
 #include "viennacl/scalar.hpp"
 #include "viennacl/vector.hpp"
 #include "viennacl/matrix.hpp"
 #include "viennacl/compressed_matrix.hpp"
 
 #define SHOW_BORDERS 1
+#define SHOW_CIRCLE 0
 #define SHOW_CENTER 1
+#define PEOPLESTATELEN 2
 
 using namespace std;
+using namespace boost::numeric;
 
 typedef float ScalarType;
 
@@ -32,16 +37,23 @@ std::default_random_engine generator;
 int width = 1024, height=768;
 unsigned char * pixels = new unsigned char[256*256 * 3];
 
-void generatePeople(unordered_map<int, int> &state){
+void generatePeople(viennacl::matrix<ScalarType> &people){
   std::uniform_int_distribution<int> position(10, 256-10);
   auto tribex = position(generator);
   auto tribey = position(generator);
+      
+      ublas::matrix<ScalarType> tmp(PEOPLESTATELEN, 10);
+
   std::uniform_int_distribution<int> distribution(-10, 10);
   for( auto i = 0; i < 10; i++){
     auto x = tribex + distribution(generator);
     auto y = tribey + distribution(generator);
-    state[x*256 + y] = PEOPLE;
+//    state[x*256 + y] = PEOPLE;
+    tmp(0, i) = x;
+    tmp(1, i) = y;
   }
+      
+      viennacl::copy(tmp, people);
 }
 void generateMinerals(unordered_map<int, int> &state){
   std::uniform_int_distribution<int> distribution(10, 256-10);
@@ -61,15 +73,29 @@ void generateMinerals(unordered_map<int, int> &state){
     state[x*256 + y] = GOLD;
   }
 }
-void updatePeople(unordered_map<int, int> &state){
+void updatePeople(unordered_map<int, int> &state, viennacl::matrix<ScalarType> &people){
   std::uniform_int_distribution<int> move(-1, 1);
   for( auto it = state.begin(); it != state.end(); ++it){
     if(it->second == PEOPLE){
-      auto dx = move(generator);
-      auto dy = move(generator);
       state[it->first] = 0;
-      state[min(255*255, max(0, it->first + dx + dy*256))] = PEOPLE;
+//      state[min(255*255, max(0, it->first + dx + dy*256))] = PEOPLE;
     }
+  }
+  ublas::matrix<ScalarType> tmp(PEOPLESTATELEN, 10);
+  for( auto i = 0; i < 10; ++i){
+    auto dx = move(generator);
+    auto dy = move(generator);
+    tmp(0, i) = dx;
+    tmp(1, i) = dy;
+  }
+  viennacl::matrix<ScalarType> people_move(PEOPLESTATELEN, 10);
+  viennacl::copy(tmp, people_move);
+  people = people + people_move;
+  viennacl::copy(people, tmp);
+  for( auto i = 0; i < 10; ++i){
+    float x = tmp(0, i);
+    float y = tmp(1, i);
+    state[min(255*255, max(0, int(x + y*256)))] = PEOPLE;
   }
 }
 
@@ -84,16 +110,17 @@ class World {
       viennacl::vector<ScalarType> vcl_vec1(vec1.size());
       viennacl::vector<ScalarType> vcl_vec2(vec1.size());
       viennacl::compressed_matrix<ScalarType, 1> vcl_compressed_matrix_1(vec1.size(), vec1.size());
+      ublas::matrix<ScalarType> tmp(vec1.size(), vec1.size());
       for(int i = 0; i < M; i++){
         vcl_vec2(i) = 0;
         for(int j = 0; j < M; j++){
-          vcl_compressed_matrix_1(i, j) = 0;
-          if(i == j){ vcl_compressed_matrix_1(i, j) = 1.0005; }
+          tmp(i, j) = 1.0005*(i == j);
         }
       }
       vcl_vec2(5) = 1;
+      viennacl::copy(tmp, vcl_compressed_matrix_1);
 
-      for(int k = 0; k < 10000; k++){
+      for(int k = 0; k < 100; k++){
         vcl_vec1 = viennacl::linalg::prod(vcl_compressed_matrix_1, vcl_vec2);
         for(int i = 4; i < 6; i++){
           cout << vcl_vec1(i) << endl;
@@ -107,7 +134,7 @@ class World {
 
     World(){
       generateMinerals(this->state);
-      generatePeople(this->state);
+      generatePeople(this->people);
       this->state[0] = 0;
       start = chrono::system_clock::now();
 
@@ -117,7 +144,7 @@ class World {
     void update(){
       auto end = chrono::system_clock::now();
       float dt = chrono::duration_cast<chrono::microseconds>(end - start).count()*0.000001;
-      updatePeople(this->state);
+      updatePeople(this->state, this->people);
       start = end;
       this->state[0] += 100*dt;
     }
@@ -153,15 +180,18 @@ class World {
             break;
         }
       }
-      auto px = 300*sin(this->state[0]*0.1);
-      auto py = 300*cos(this->state[0]*0.1);
+      //Draw circle
       auto cp = 3*((int)(3*width * height/2 + 3*width/2)/3);
-      for (auto i = 0; i < 100; i++) {
-        int x = 3*((int)((20*sin(i*0.063) + px)/3));
-        int y = 3*((int)((20*cos(i*0.063) + py)/3));
-        pixels[cp + x*width + y] = 0xFF;
-        pixels[cp + x*width + y + 1] = 0xFF;
-        pixels[cp + x*width + y + 2] = 0xFF;
+      if(SHOW_CIRCLE){
+        auto px = 300*sin(this->state[0]*0.1);
+        auto py = 300*cos(this->state[0]*0.1);
+        for (auto i = 0; i < 100; i++) {
+          int x = 3*((int)((20*sin(i*0.063) + px)/3));
+          int y = 3*((int)((20*cos(i*0.063) + py)/3));
+          pixels[cp + x*width + y] = 0xFF;
+          pixels[cp + x*width + y + 1] = 0xFF;
+          pixels[cp + x*width + y + 2] = 0xFF;
+        }
       }
       //Borders
       if(SHOW_BORDERS){
@@ -199,7 +229,7 @@ class World {
   private:
      chrono::time_point<chrono::system_clock> start;
      unordered_map<int, int> state;
-
+     viennacl::matrix<ScalarType> people;
 };
 
 void updatePixels(unsigned char * pixels, int width, int height){
