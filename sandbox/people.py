9e004adc840a133eb9068852e9107856f0dd991a
Lasse Hyyrynen <hyyrynen@lingsoft.fi>
2017-10-15 09:12:44 +0300

    Add comments

--- sandbox/people.py
+++ sandbox/people.py
@@ -318,7 +318,16 @@ knowledge = 0.3
 demand0 = torch.Tensor([hunger, thirst, peace, heat, activity, space, friends, knowledge]).type(dtype)
 demand = Variable(demand0, requires_grad=False)
 
+"""
+Here we calculate the distance to everything the map offers. Then we use an
+exotic mean-type of function together with the demand-values to find which
+areas of the map are most attractive.
 
+We define happiness as the sum of supply attractions, so that we are more 
+happy the closer we are to what themap offers for our needs. To find out were
+we should go, we calculate the gradient with regards to our current position
+and then we move that way.
+"""
 powermean = lambda arr, k: (arr).pow(k).sum(0)/(arr).pow(k - 1).sum(0)
 for i in range(10):
   supplydistances = (supply - pos).pow(2).sum(2).t().sqrt()
@@ -330,3 +339,15 @@ for i in range(10):
   print(pos)
   print(happiness)
 
+
+"""
+Next we should reduce the actors knowledge by hiding everything that are not
+in straight line of sight and what the actor doesn't remember. Then we should
+generalize the above code to run on multiple actors. We should also reduce
+calculations by disregarding everything outside some bounding box.
+
+To make the actor more complete, we should look into making the actor satisfie
+its needs when it is close enought to the supplies. So when the actor is close
+to water and it is thirsty, after spending a moment there the thirsty-value
+starts to go to 0.
+"""
